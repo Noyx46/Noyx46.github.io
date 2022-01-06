28942cd5e4e541625b1013772613b638b24ba69b
Noyx46 <Noyx_406@protonmail.com>
2022-01-05 20:06:34 -0500

    Redo chess highlighting; use JQuery; change chess board to background image

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,86 +1,51 @@
-const files = "abcdefgh"
-const ranks = "87654321"
+let hltiles = []
 
-// Create the chess grid
-let grid = document.getElementsByClassName("game-grid")[0];
+$(".game-board").contextmenu((e) => {
+  e.preventDefault();
 
-// There are 8 rows
-for (let row = 0; row < 8; row++) {
-  let gameRow = document.createElement("div");
-  gameRow.classList.add("game-row");
-  grid.append(gameRow);
+  // Get mouse position
+  let offset = $(".game-board").offset();
+  let mouseX = e.pageX - offset.left;
+  let mouseY = e.pageY - offset.top;
 
-  // Each row contains 8 tiles
-  for (let col = 0; col < 8; col++) {
-    let gameTile = document.createElement("div");
-    gameTile.classList.add("game-tile");
-    gameTile.classList.add("code");
-    gameTile.innerHTML = `${files[col]}${ranks[row]}`;
-    gameTile.style.textAlign = "center";
-    gameTile.style.lineHeight = "80px";
-
-    // Add right-click highlighting action
-    gameTile.addEventListener("contextmenu", (e) => {
-      // Prevent opening a menu
-      e.preventDefault();
-
-      let hlColor = "var(--red)";
-      // Use a different color for each key;
-      if (e.ctrlKey) {
-        if (e.altKey) {
-          hlColor = "var(--green)";
-        } else {
-          hlColor = "var(--blue)";
-        }
-      } else if (e.altKey) {
-        hlColor = "var(--yellow)";
-      }
-
-      if (gameTile.style.backgroundColor == hlColor) {
-        let parent = gameTile.parentElement;
-
-        let findRow = 0;
-        let findCol = 0;
-
-        // find parent element in grid for row
-        for (findRow = 0; findRow < 8; findRow++) {
-          if (grid.children[findRow] == parent) {
-            break;
-          }
-        }
-
-        // find self in parent for column
-        for (findCol = 0; findCol < 8; findCol++) {
-          if (parent.children[findCol] == gameTile) {
-            break;
-          }
-        }
-
-        // white if row + column is even
-        if ((findRow + findCol) % 2 == 0) {
-          gameTile.style.backgroundColor = "var(--grey2)";
-        } else {
-          gameTile.style.backgroundColor = "var(--grey0)";
-        }
-      } else {
-        gameTile.style.backgroundColor = hlColor;
-      }
-    })
-
-    gameRow.append(gameTile);
-  }
-}
-
-// Show coordinates on each game tile
-function showCoords() {
-
-}
-
-// Hide coordinates on each game tile
-function hideCoords() {
-  for (let rowDiv of grid.children) {
-    for (let tile of rowDiv.children) {
-      tile.innerHTML = "";
+  let color = "--red";
+  if (e.altKey) {
+    if (e.ctrlKey) {
+      color = "--green";
+    } else {
+      color = "--yellow";
     }
+  } else if (e.ctrlKey) {
+    color = "--blue";
   }
+
+  highlightTile(Math.floor(mouseX / 80), Math.floor(mouseY / 80), color);
+})
+
+$(".game-board").click((e) => {
+  $(".highlight-tile").remove();
+})
+
+// Create a highlight at a certain coordinate
+// x: x-coordinate on board
+// y: y-coordinate on board
+// c: color variable ("--red")
+function highlightTile(x, y, c) {
+  // Check for highlighted tile in position
+  if ($(`.cco-${x}${y}`).length) {
+    $(`.cco-${x}${y}`).remove();
+    return;
+  }
+
+  let color = getComputedStyle($("body").get(0)).getPropertyValue(c || "--red");
+  let xpx = `${x * 80}px`;
+  let ypx = `${y * 80}px`;
+  let hl = $("<div>", {
+    width: "80px",
+    height: "80px",
+  }).css("background-color", color + "cc")
+    .css("transform", `translate(${xpx}, ${ypx})`)
+    .addClass("highlight-tile")
+    .addClass(`cco-${x}${y}`);
+  $(".game-board").append(hl);
 }
