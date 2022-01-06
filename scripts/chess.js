f062d9a2ebd0f801d7bcb2c698cc6b8ce067d74a
Noyx46 <Noyx_406@protonmail.com>
2022-01-05 22:08:08 -0500

    Allow changing tile highlight color with one click

--- scripts/chess.js
+++ scripts/chess.js
@@ -8,15 +8,15 @@ $(".game-board").contextmenu((e) => {
   let mouseX = e.pageX - offset.left;
   let mouseY = e.pageY - offset.top;
 
-  let color = "--red";
+  let color = "red";
   if (e.altKey) {
     if (e.ctrlKey) {
-      color = "--green";
+      color = "green";
     } else {
-      color = "--yellow";
+      color = "yellow";
     }
   } else if (e.ctrlKey) {
-    color = "--blue";
+    color = "blue";
   }
 
   highlightTile(Math.floor(mouseX / 80), Math.floor(mouseY / 80), color);
@@ -29,15 +29,19 @@ $(".game-board").click((e) => {
 // Create a highlight at a certain coordinate
 // x: x-coordinate on board
 // y: y-coordinate on board
-// c: color variable ("--red")
+// c: color variable ("red")
 function highlightTile(x, y, c) {
   // Check for highlighted tile in position
   if ($(`.cco-${x}${y}`).length) {
-    $(`.cco-${x}${y}`).remove();
-    return;
+    if ($(`.cco-${x}${y}.hl-${c}`).length) {
+      $(`.cco-${x}${y}.hl-${c}`).remove();
+      return;
+    } else {
+      $(`.cco-${x}${y}`).remove();
+    }
   }
 
-  let color = getComputedStyle($("body").get(0)).getPropertyValue(c || "--red");
+  let color = getComputedStyle($("body").get(0)).getPropertyValue(`--${c}` || "--red");
   let xpx = `${x * 80}px`;
   let ypx = `${y * 80}px`;
   let hl = $("<div>", {
@@ -46,6 +50,7 @@ function highlightTile(x, y, c) {
   }).css("background-color", color + "cc")
     .css("transform", `translate(${xpx}, ${ypx})`)
     .addClass("highlight-tile")
-    .addClass(`cco-${x}${y}`);
+    .addClass(`cco-${x}${y}`)
+    .addClass(`hl-${c}`);
   $(".game-board").append(hl);
 }
